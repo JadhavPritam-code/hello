b84aff4c35036e1c0b0c21f916ab5edaa183aa54
JadhavPritam-code <jadhavpritam5303@gmail.com>
2025-10-08 09:41:18 +0530

    javascipt.js

---
+++ javascipt.js
@@ -0,0 +1,11 @@
+<html>
+<head>
+  <title>Simple JavaScript Example</title>
+  <script src="javascript.js" defer></script>
+</head>
+<body>
+  <h1>Hello JavaScript</h1>
+  <p id="message">This is a message.</p>
+  <button onclick="changeText()">Click Me</button>
+</body>
+</html>
